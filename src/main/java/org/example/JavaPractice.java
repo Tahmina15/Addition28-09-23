19fcc9f6639293e94e409e433f5d611092b0654c
tahmina <liza.tahmina15@gmail.com>
2023-10-26 21:52:38 +0100

    Addition commit.

---
+++ src/main/java/org/example/JavaPractice.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class JavaPractice {
+    public static void main(String[]args){
+      int num1=100;
+      int num2=200;
+      System.out.println(num1+num2);
+
+
+
+
+    }
+}
